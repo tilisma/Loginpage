e27b2ae0fca33f78c8853416ebe6077fd9a0e531
tilisma <tilisma123.pahari@gmail.com>
2023-10-06 10:18:29 +0545

    update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
 function Login() {
-  return (
-    <div>Login</div>
+  return ( 
+    <div class="login-container">  
+    <form>
+
+    <div class="form-group"> 
+        <label For="username">Username</label> 
+        <input type="text" id="username" name="username"/> 
+    </div> 
+    <div class="form-group"> 
+        <label For="password">Password</label> 
+        <input type="password" id="password" name="password"/> 
+    </div> 
+    <button type="submit">Submit</button> 
+    </form>
+      </div>
   )
 }
 
-export default Login
+export default Login 
